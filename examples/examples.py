c8bce69f81490ccef80327c318ecf0f816439488
jixiang <JIXIANG@kingsoft.com>
2018-03-27 11:46:54 +0800

    update examples/README.md

--- examples/examples.py
+++ examples/examples.py
@@ -6,7 +6,8 @@ krds_client = KRDSClient('your_ak', 'your_sk', 'your_service_region')
 #
 # r = krds_client.CreateDBInstance(DBInstanceClass='db.ram.1|db.disk.10', DBInstanceName='test', Engine='mysql',
 #                                  EngineVersion='5.6', MasterUserName='test', MasterUserPassword='Test123456',
-#                                  DBInstanceType=DBInstanceType.HA, VpcId='b33a2276-64a8-4c04-b28e-da253c8add32',
+#                                  DBInstanceType=DBInstanceType.HA, PubliclyAccessible=True,
+#                                  VpcId='b33a2276-64a8-4c04-b28e-da253c8add32',
 #                                  SubnetId='c2e0abd7-13df-461a-bd8d-3b92faebf111', BillType=BillType.DAY)
 # r = krds_client.DescribeDBInstances(None, 'HA', None, 'ACTIVE')
 # r = krds_client.CreateDBInstanceReadReplica("test-rr", "d4fe1bcf-a99a-4b1e-ba61-3c751b650249")
